77187314af59f1312078fd90d4132a5e984640e3
Stewart Chan <stew.chan@gmail.com>
2024-10-18 11:51:12 -0400

    Move with right arrow

--- core/src/main/java/io/github/some_example_name/Main.java
+++ core/src/main/java/io/github/some_example_name/Main.java
@@ -2,6 +2,7 @@ package io.github.some_example_name;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -9,6 +10,7 @@ import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 
 /**
  * {@link com.badlogic.gdx.ApplicationListener} implementation shared by all
@@ -25,6 +27,8 @@ public class Main extends ApplicationAdapter {
     Sound dropSound;
     Music music;
 
+    Sprite bucketSprite;
+
     @Override
     public void create() {
         batch = new SpriteBatch();
@@ -35,6 +39,8 @@ public class Main extends ApplicationAdapter {
         backgroundTexture = new Texture("background.png");
         bucketTexture = new Texture("bucket.png");
         dropTexture = new Texture("drop.png");
+        bucketSprite = new Sprite(bucketTexture);
+        bucketSprite.setSize(1, 1);
 
         dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.mp3"));
         music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
@@ -64,7 +70,12 @@ public class Main extends ApplicationAdapter {
     }
 
     private void input() {
+        float speed = 4f;
+        float delta = Gdx.graphics.getDeltaTime();
 
+        if (Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)) {
+            bucketSprite.translateX(speed * delta);
+        }
     }
 
     private void logic() {
@@ -80,7 +91,7 @@ public class Main extends ApplicationAdapter {
         float worldHeight = viewport.getWorldHeight();
 
         batch.draw(backgroundTexture, 0, 0, worldWidth, worldHeight);
-        batch.draw(bucketTexture, 0, 0, 1, 1);
+        bucketSprite.draw(batch);
 
         batch.end();
     }
