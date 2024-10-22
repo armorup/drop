0fad8b0ed0e79bbc6d8fa562a0882da61f2f385b
Stewart Chan <stew.chan@gmail.com>
2024-10-22 10:06:23 -0400

    Add ttf font support

--- core/src/main/java/ca/codepet/drop/Drop.java
+++ core/src/main/java/ca/codepet/drop/Drop.java
@@ -1,8 +1,10 @@
 package ca.codepet.drop;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 
 public class Drop extends Game {
 
@@ -11,7 +13,15 @@ public class Drop extends Game {
 
   public void create() {
     batch = new SpriteBatch();
-    font = new BitmapFont(); // use libGDX's default Arial font
+    font = new BitmapFont(); // default Arial font
+
+    // Generate a font from TTF
+    FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/Play-Regular.ttf"));
+    FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+    parameter.size = 24; // font size
+    font = generator.generateFont(parameter); // Generate BitmapFont from TTF
+    generator.dispose();
+
     this.setScreen(new MainMenuScreen(this));
   }
 
